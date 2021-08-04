a2aa4c9aec00e0abf4b9c16fea4ef76dcf0b91a8
naisannovel <naisannovel@gmail.com>
2021-08-04 23:29:25 +0600

    update

--- operator.py
+++ operator.py
@@ -12,10 +12,43 @@ print(20 % 10)    # 0
 print(3**3)       # 3 to the power 3. 3*3*3=27
 
 # relational or comparison operator
-
+# >, <, ==, <=, >=, !=
 print(10 > 20)      # false
 print(10 < 20)      # true
 print(10 == 20)     # false
 print(10 <= 20)     # true
 print(10 >= 20)     # false
 print(10 != 20)     # true
+
+# logical operator
+# and, or, not
+print(10 > 5 and 5 < 9)
+# true
+print(10 < 5 or 15 > 7)
+# true
+print(not 10 > 20)
+# true
+
+# assignment operator
+# =
+age = 24
+age += 1
+print(age)
+# 25
+age -= 1
+print(age)
+# 24
+
+# identity operator
+# is, in
+n = 10
+m = 10
+o = 20
+print(n is m)   # true
+print(n is o)   # false
+print(n is not o)   # true
+
+name = 'naisan'
+print('n' in name)  # true
+print('m' in name)  # false
+print('p' not in name)  # true
